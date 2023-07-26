f29f763fda905aa0c3c6bcaa9674f209673fac22
Kingtang <kingtang2180@gmail.com>
2023-07-26 16:29:58 +0800

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
+
 const dbConfig = require("../config/db.js");
-=======
-const dbConfig = require("../db/db.js");
->>>>>>> 5742268fe11c8d2d45e3fc4296258ec6684424f3
+
+
 
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
@@ -10,7 +9,7 @@ mongoose.Promise = global.Promise;
 const db = {}; 
 db.mongoose = mongoose;
 db.url = dbConfig.url;
-<<<<<<< HEAD
+
 
 
 db.user = require("./user.model.js");
@@ -19,8 +18,7 @@ db.role = require("./role.model.js");
 
 db.tutorials = require("./tutorial.model.js")(mongoose);
 db.ROLES = ["user", "admin", "moderator"];
-=======
-db.tutorials = require("./tutorial.model.js")(mongoose);
->>>>>>> 5742268fe11c8d2d45e3fc4296258ec6684424f3
+
+
 
 module.exports = db;
